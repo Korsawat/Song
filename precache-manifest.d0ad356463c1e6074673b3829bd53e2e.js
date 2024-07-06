3b8ef38ee3d5306200cc84251ffae5771715fb04
Korsawat Baiseejuang <korsawat.bai@student.mahidol.edu>
2024-07-07 00:57:56 +0700

    Updates

---
+++ precache-manifest.d0ad356463c1e6074673b3829bd53e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e26d4d2e66cb48dd4bc32899cd3e541",
+    "url": "/Song/index.html"
+  },
+  {
+    "revision": "39df22d617794c1ccbdd",
+    "url": "/Song/static/css/main.ee0c9ee2.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/Song/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Song/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "39df22d617794c1ccbdd",
+    "url": "/Song/static/js/main.6bd7c7e5.chunk.js"
+  },
+  {
+    "revision": "5765267f2eec8840c725",
+    "url": "/Song/static/js/runtime~main.1e2cdb72.js"
+  }
+]);
